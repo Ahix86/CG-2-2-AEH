51ff7241ba092d8018e020007df5129123d1c7a4
Allison <allison.hix86@gmail.com>
2018-07-17 19:56:50 -0500

    Area of a rectangle

---
+++ CG 2-2/CG 2-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CG_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the area of a rectangle");
+            Console.Write("Please enter the length of the rectangle:  ");
+            string length = Console.ReadLine();
+            
+
+            Console.Write("Please enter the width of the rectangle:  ");
+            string width = Console.ReadLine();
+
+            int area = Convert.ToInt32(length) * Convert.ToInt32(width);
+
+            Console.WriteLine("The area of the rectangle is " + area + ".");
+            Console.ReadLine();
+        }
+    }
+}
